13c2f6370fdccc6022f85dc2c53be29b3855d582
ArisThm <thomas.aristeids@gmail.com>
2021-12-30 16:46:48 +0200

    Round 1 Complete

--- Rock Paper Scissors Game/script.js
+++ Rock Paper Scissors Game/script.js
@@ -1,20 +1,53 @@
-function round1() { 
-    function playerChoice() {
-        let playerSelection = prompt("Pick Rock, Paper or Scissors");
-        return playerSelection;
-        //console.log(playerSelection);
+function game() { 
 
+    function round1() {
+
+        let playerChoice = prompt("rock, paper or scissors").toLowerCase();
+        let computerChoice = computerPICK();
+                
+        function computerPICK() {
+            let random = ["rock", "paper", "scissors"];
+            return random[Math.floor(Math.random() * 3)];
+        }
+    
+        if (playerChoice === "rock") {
+            if (playerChoice === "rock" && computerChoice === "paper") {
+                return "You lose! Paper Beats Rock";
+            } else if (playerChoice === "rock" && computerChoice === "rock") {
+                return "Draw!";
+            } else if (playerChoice === "rock" && computerChoice === "scissors") {
+                return "You won! Rock beats scissors!";
+            } else {
+                return "lathos";
+            }
+        } else if (playerChoice === "paper") {
+            if (playerChoice === "paper" && computerChoice === "paper") {
+                return "Draw!";
+            } else if (playerChoice === "paper" && computerChoice === "rock") {
+                return "You win! Paper beats scissors";
+            } else if (playerChoice === "paper" && computerChoice === "scissors") {
+                return "You lose! Scissors beat paper";
+            } else {
+                return "lathos"
+            }
+        } else if (playerChoice === "scissors") {
+            if (playerChoice === "scissors" && computerChoice === "scissors") {
+                return "Draw!";
+            } else if (playerChoice === "scissors" && computerChoice === "paper") {
+                return "You win! Scissors beat paper";
+            } else if (playerChoice === "scissors" && computerChoice === "rock") {
+                return "You lose! Rock beats scissors";
+            } else {
+                return "lathos"
+            }
+        } else {
+            return "lathos"
+        }
     }
-
-   playerChoice();
-
-   function computerChoice() {
-       let random = ["rock", "paper", "scissors"];
-       return random[Math.floor(Math.random() * 3)];
-
-    }
-   computerChoice();
- 
+    console.log(round1());
+    console.log(round1());
+    console.log(round1());
+    console.log(round1());
+    console.log(round1());
 }
-
-round1();
+game();
